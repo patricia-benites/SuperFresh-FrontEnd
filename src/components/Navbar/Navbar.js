becab444b0cc85a44ce29089c809ceaf09fd02c3
Patricia <patricia.benites.cava@gmail.com>
2022-04-14 19:27:13 +0200

    style navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import styles from './Navbar.module.css'
+import React from "react";
+import styles from "./Navbar.module.css";
+import { Search } from "@material-ui/icons";
+import {Badge} from '@material-ui/material/Badge';
 
 export default function Navbar() {
   return (
-    <div className={styles.container}>Navbar</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.leftContainer}>left</div>
+      <div className={styles.searchContainer}>
+      {<Search></Search>}
+      </div>
+      <div className={styles.rightContainer}>
+      {<Badge badgeContent={4} color="primary"></Badge>}
+      right</div>
+    </div>
+  );
 }
