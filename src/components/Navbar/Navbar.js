69fa1fb97bd706881f2bc02dddac320409c9ad6f
Patricia <patricia.benites.cava@gmail.com>
2022-04-14 20:17:10 +0200

    update right container navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,21 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-import { Search } from "@material-ui/icons";
-import {Badge} from '@material-ui/material/Badge';
+
 
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      <div className={styles.leftContainer}>left</div>
+      <div className={styles.leftContainer}>
+        <h2>SuperFresh</h2>
+      </div>
       <div className={styles.searchContainer}>
-      {<Search></Search>}
+        <input type="text" placeholder="Search ...."></input>
       </div>
       <div className={styles.rightContainer}>
-      {<Badge badgeContent={4} color="primary"></Badge>}
-      right</div>
+        <p>Signup</p>
+        <p>Login</p>
+        <p>Cart</p>
+      </div>
     </div>
   );
 }
