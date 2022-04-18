ac04420011600b4eb0c142fb0e1348107e068051
Patricia <patricia.benites.cava@gmail.com>
2022-04-18 16:34:36 +0200

    connect cart page to backend

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import { Announcement } from '../../components/Announcement/Announcement'
+import { Footer } from '../../components/Footer/Footer'
 import Navbar from '../../components/Navbar/Navbar'
+// import { AuthContext } from "../../context/context";
+// import { useContext } from "react";
+import { useEffect, useState } from "react";
+import { client } from "../../client";
 
 export function Cart() {
+  const [cart, setCart] = useState([]);
+
+  const getCart = async () => {
+    const result = await client.get("/carts/find");
+    setCart(result.data);
+  };
+
+  useEffect(() => {
+    getCart();
+  }, []);
+
+
   return (
     <div>
         <Announcement/>
         <Navbar/>
         <div className='cartContainer'>
         <h2>Your Shopping Cart</h2>
+        <div className='leftContainer'>
+        <code>{JSON.stringify(cart)}</code>
+          
         </div>
-        <h2>Cart</h2>
-        <h3>Footer</h3>
+        <div className='rightContainer'></div>
+        </div>
+        <Footer/>
     </div>
   )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,17 @@
 import React from "react";
 import { Announcement } from "../../components/Announcement/Announcement";
 import Navbar from "../../components/Navbar/Navbar";
-import { useContext } from "react";
-import { AuthContext } from "../../context/context";
 import { ListOfProducts } from "../../components/ListOfProducts/ListOfProducts";
 import { useEffect, useState } from "react";
 import { client } from "../../client";
 import { Footer } from "../../components/Footer/Footer";
 
 export default function Home() {
-  const { user } = useContext(AuthContext);
   const [products, setProducts] = useState([]);
 
   const getProducts = async () => {
     const result = await client.get("/products/find");
     setProducts(result.data);
-    console.log(result.status)
-    console.log(products);
   };
 
   useEffect(() => {
