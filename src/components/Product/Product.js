c097faa16b59b3394f3697d2e7393558ac3f98ac
Patricia <patricia.benites.cava@gmail.com>
2022-04-19 21:17:16 +0200

    cleaning code

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,36 +7,35 @@ import { AuthContext } from "../../context/context";
 
 export function Product({ id, image, title, description, price }) {
   const { user } = useContext(AuthContext);
-  console.log(user._id)
 
   const addToCart = async () => {
     const existingCart = await client.get(`/carts/find/${user._id}`)
-    const cart = existingCart.data
-    console.log(cart)
-    const products = cart.products
-    console.log(products)
-    products.push({"productId":`${id}`})
-    if (cart) {
-      const result = await client.patch(`/carts/${cart._id}`, { "products": products})
-      console.log(result.status);
+    if (existingCart) {
+      const products = existingCart.data.products
+      products.push({"productId":`${id}`})
+      const result = await client.patch(`/carts/${existingCart.data._id}`, { "products": products})
+    } else {
+      const result = await client.post("/carts", {"userId":user._id, "products":[{"productId":`${id}`}]})
     }
-    // const result = await client.post("/products/find");
+
+    }
+
+    return (
+      <div className={styles.productContainer}>
+        <div className={styles.imageContainer}>
+          <img src={image} alt={image}></img> 
+        </div>
+        <div className={styles.infoContainer}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <p>{` € ${price}`}</p>
+        </div>
+        <div className={styles.iconsContainer}>
+          <AiOutlineShoppingCart size="1em" onClick={addToCart}/>
+          <AiOutlineHeart/>
+        </div>
+      </div>
+    );
   }
 
-  return (
-    <div className={styles.productContainer}>
-      <div className={styles.imageContainer}>
-        <img src={image} alt={image}></img> 
-      </div>
-      <div className={styles.infoContainer}>
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <p>{` € ${price}`}</p>
-      </div>
-      <div className={styles.iconsContainer}>
-        <AiOutlineShoppingCart size="1em" onClick={addToCart}/>
-        <AiOutlineHeart/>
-      </div>
-    </div>
-  );
-}
+  
