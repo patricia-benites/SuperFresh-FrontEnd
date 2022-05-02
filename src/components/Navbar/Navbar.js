a1afdb2dc565bdea5c9c1bb3e4c4381baf464341
Patricia <patricia.benites.cava@gmail.com>
2022-05-02 20:00:54 +0200

    change visibility of navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import {AiOutlineShoppingCart, AiOutlineSearch} from "react-icons/ai"
+import { useContext } from "react";
+import { AuthContext } from "../../context/context";
 
 export default function Navbar() {
+  const { user } = useContext(AuthContext);
   return (
     <div className={styles.navbarContainer}>
       <div className={styles.wrapper}>
@@ -14,12 +17,12 @@ export default function Navbar() {
             <AiOutlineSearch style={{color:"gray", fontSize: "16px"}}/>
           </div>
           <div className={styles.rightContainer}>
-            <div className="user">
+            {user && <div className="user">
               <span>Hello </span>
-              <span>Patricia</span>
-            </div>
-            <Link to='/signup' style={{color:"black"}} >REGISTER</Link>
-            <Link to='/login' style={{color:"black"}}>LOGIN</Link> 
+              <span>{user.firstName}</span>
+            </div>}
+            {!user && <Link to='/signup' style={{color:"black"}} >REGISTER</Link>}
+            {!user && <Link to='/login' style={{color:"black"}}>LOGIN</Link> }
             <Link to='/cart'><AiOutlineShoppingCart size="1.3em" style={{color:"black"}}/></Link>  
           </div>
       </div>
